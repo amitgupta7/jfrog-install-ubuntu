0420ff587651c3321dac2cbba95e062eeabbd931
Amit Gupta <amit864@gmail.com>
2025-08-20 16:40:02 +0530

    updated to point artifactory directory to /data

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ echo "deb https://releases.jfrog.io/artifactory/artifactory-pro-debs $DIST main"
 wget -qO - https://releases.jfrog.io/artifactory/api/v2/repositories/artifactory-pro-debs/keyPairs/primary/public | sudo apt-key add -
 sudo apt-get install -y net-tools
 sudo apt-get update && sudo apt-get install jfrog-artifactory-pro=7.111.11
+mkdir -p /data/artifactory/var
+mv /opt/jfrog/artifactory/var /opt/jfrog/artifactory/var.bak
+sudo ln -s /data/artifactory/var /opt/jfrog/artifactory/var
+sudo chown -h artifactory:artifactory /data/artifactory/var
+sudo chown -h artifactory:artifactory /opt/jfrog/artifactory/var
+sudo systemctl start artifactory.service
