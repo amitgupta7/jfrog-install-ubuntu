cbedc15ba2aab3689a103452a4d7d7684c3fd23a
Amit Gupta <amit864@gmail.com>
2025-08-20 17:07:20 +0530

    use jfrog oss

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 JFROG_HOME=/data/jfrog
-DIST=$(lsb_release -c | grep Codename | awk '{print $2}')
-#echo "deb https://releases.jfrog.io/artifactory/artifactory-pro-debs $DIST main" | sudo tee -a /etc/apt/sources.list
-#wget -qO - https://releases.jfrog.io/artifactory/api/v2/repositories/artifactory-pro-debs/keyPairs/primary/public | sudo apt-key add -
+DIST=$(lsb_release -cs)
+echo "deb https://releases.jfrog.io/artifactory/artifactory-debs $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list
+wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add - 
 sudo apt-get install -y net-tools
 sudo apt-get update && sudo apt-get install -y jfrog-artifactory-oss 
 sudo systemctl restart artifactory.service
